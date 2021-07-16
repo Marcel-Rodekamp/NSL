b411287c7fe312bd158485ddecb690226efb63e1
Marc Túnica <mtuicro18@alumnes.ub.edu>
2021-07-16 15:39:00 +0200

    Implemented tensor functions.

--- Executables/Examples/example_tensor.cpp
+++ Executables/Examples/example_tensor.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
 #include "Tensor/tensor.hpp"
+#include <vector>
 
 int main(){
     // test you tensors here if you like ;)
 
-    NSL::Tensor<float> example_tensor (12);
+//Examples for tensor class
+    //Example for tensor construction, 1D, 3D and copy. Must change data_ from private to public.
+ /*  NSL::Tensor<float> example_tensor0 (12);
+    std::cout<< example_tensor0.data_ << std::endl;
+    NSL::Tensor<float> example_tensor1 = example_tensor0;
+    std::cout<< example_tensor1.data_ << std::endl;
+    std::vector<long int> dimension = {60,60,12};
+    NSL::Tensor<float> example_tensor2 (dimension);
+    std::cout<< example_tensor2.data_ << std::endl;*/
+
+    //Example of the use of SHAPE.
+    /*NSL::Tensor<float> example_tensor3 ({60,60,12});
+    std::size_t a = 1;
+    std::cout<<example_tensor3.shape(a);*/
+
+    //Example of the use of random access.
+    /*NSL::Tensor<float> example_tensor1 ({60,60,12});
+    long int a = 1;
+     std::cout<< example_tensor1(a,a,a) <<std::endl;*/
+
+    //Example of the use of exponential
+   /* NSL::Tensor<float> example_tensor5 ({60,60,12}); //Initialize tensor
+    NSL::Tensor<float> example_tensor6 = example_tensor5.exponential();
+    std::cout<< example_tensor6.data_<<std::endl;
+
+    NSL::Tensor<float> example_tensor7 = example_tensor6.exponential();
+    std::cout<< example_tensor7.data_<<std::endl;*/
+
+//-------------------------------------------------------------------------------
+// Example of Time Tensor
 }
--- src/NSL/Tensor/tensor.hpp
+++ src/NSL/Tensor/tensor.hpp
@@ -2,8 +2,10 @@
 #define NSL_TENSOR_HPP
 
 #include<torch/torch.h>
-
 #include <memory>
+#include<vector>
+
+using namespace torch::indexing;
 
 // ============================================================================
 // CPU Implementations
@@ -12,41 +14,70 @@ namespace NSL {
 template<typename Type>
 class Tensor {
     private:
-        torch::tensor data_;
+    //torch::Tensor data_;
 
     public:
+        torch::Tensor data_; //just to prove cout's in the main program.
+
+        //Default constructor
+        explicit Tensor()= default;
+
         // construct 1D tensor (i.e. array)
         explicit Tensor(std::size_t size){
-            data_ = torch::zeros({size});
+            data_ = torch::zeros({static_cast<const long int>(size)});
         }
 
         // construct data_ with sizes stored in dims
-        explicit Tensor(std::initializer_list<std::size_t> dims);
+        explicit Tensor(std::vector< long int> dims){
+            data_= torch::zeros(dims);
+        }
 
         // copy constructor
-        Tensor(Tensor<Type> & other);
+        Tensor(Tensor<Type> & other){
+            data_ = other.data_;
+        }
 
         // random access operator
-        Type &operator()(std::size_t idx);
+        //ToDo: Must be reference to return.
+        Type  operator()( std::vector<long int> idx){
+            return data_.index(idx).item<Type>();
+        }
 
         // return size of dimension in data_
-        const std::size_t shape(const std::size_t dim);
+        std::size_t shape(std::size_t dim){
+            return data_.size(static_cast<const long int>(dim));
+        }
+
+        //Tensor exp
+        Tensor<Type> exp(){
+            Tensor<Type> exp;
+            exp.data_ = torch::exp(data_);
+            return exp;
+        }
+
 };
 
 template<typename Type>
 class TimeTensor {
     private:
-    // xt::xtensor data_;
+    //        torch::Tensor data_;
 
     public:
+    torch::Tensor data_; //just to prove cout's in the main program.
+
     // construct 1D tensor (i.e. array)
-    explicit TimeTensor(std::size_t size);
+    explicit TimeTensor(std::size_t size){
+        data_ = torch::zeros({static_cast<const long int>(size)});
+    };
 
     // construct data_ with sizes stored in dims
-    explicit TimeTensor(std::initializer_list<std::size_t> dims);
+    explicit TimeTensor(std::initializer_list<std::size_t> dims){
+    };
 
     // copy constructor
-    TimeTensor(Tensor<Type> & other);
+    TimeTensor(Tensor<Type> & other){
+        data_ = other.data_;
+    };
 
     // random access operator
     Type &operator()(std::size_t idx);
