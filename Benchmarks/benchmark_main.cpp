2826110329b708a511c34093a46cfe3de2f81165
Marcel Rodekamp <marcel.rodekamp@gmail.com>
2021-09-03 11:48:52 +0200

    Catch2 requires a main, without kokkos we can just autogenerate it by catch2

--- Benchmarks/benchmark_main.cpp
+++ Benchmarks/benchmark_main.cpp
@@ -1,15 +1,15 @@
-#define CATCH_CONFIG_RUNNER
+#define CATCH_CONFIG_MAIN
 #define CATCH_CONFIG_ENABLE_BENCHMARKING
 
 #include "catch2/catch.hpp"
-#include <Kokkos_Core.hpp>
+//#include <Kokkos_Core.hpp>
 
-int main( int argc, char* argv[] ) {
-    // ToDo: We should hide Kokkos::initialization in a NSL::initialization()!
-    Kokkos::initialize(argc, argv);
-
-    int result = Catch::Session().run( argc, argv );
-
-    Kokkos::finalize();
-    return result;
-}
+//int main( int argc, char* argv[] ) {
+//    // ToDo: We should hide Kokkos::initialization in a NSL::initialization()!
+//    Kokkos::initialize(argc, argv);
+//
+//    int result = Catch::Session().run( argc, argv );
+//
+//    Kokkos::finalize();
+//    return result;
+//}
