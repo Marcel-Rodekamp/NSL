a4a3acbb45ec122edeb0a82003958854dbfbaa03
Evan Berkowitz <git@evanberkowitz.com>
2021-09-07 18:07:47 +0200

    cmake: handle Apple special case
     - .dylib instead of .so
     - fetching the apple-specific version
     - automatically setting Torch_DIR relative to the _deps.

--- cmake/torch.cmake
+++ cmake/torch.cmake
@@ -1,14 +1,27 @@
-FetchContent_Declare(
-    Torch
-    URL https://download.pytorch.org/libtorch/nightly/cpu/libtorch-shared-with-deps-latest.zip
-)
+if(APPLE)
+    message(STATUS "Getting Torch: Apple CPU")
+    FetchContent_Declare(
+        Torch
+        URL https://download.pytorch.org/libtorch/nightly/cpu/libtorch-macos-latest.zip
+    )
+else()
+    message(STATUS "Getting Torch: Linux CPU")
+    FetchContent_Declare(
+        Torch
+        URL https://download.pytorch.org/libtorch/nightly/cpu/libtorch-cxx11-abi-shared-with-deps-latest.zip
+    )
+endif()
+
 
 FetchContent_MakeAvailable(Torch)
 
+set(Torch_DIR "${FETCHCONTENT_BASE_DIR}/torch-src/share/cmake/Torch")
+
 find_package(Torch REQUIRED)
 
-message(STATUS "ADDING TORCH_CXX_FLAGS: ${TORCH_CXX_FLAGS}")
-message(STATUS "ADDING TORCH_LIBRARIES: ${TORCH_LIBRARIES}")
+message(STATUS "Torch dir: ${Torch_DIR}")
+message(STATUS "Adding TORCH_CXX_FLAGS: ${TORCH_CXX_FLAGS}")
+message(STATUS "Adding TORCH_LIBRARIES: ${TORCH_LIBRARIES}")
 
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${TORCH_CXX_FLAGS}")
 target_link_libraries(NSL
