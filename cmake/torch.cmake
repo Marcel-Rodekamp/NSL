21bd518b0f3fd7b041406f45a46feede143477a3
Marcel Rodekamp <marcel.rodekamp@gmail.com>
2021-09-13 10:51:28 +0200

    cmake: Added automatic detection if Torch is installed. Will only be downloaded if not.

--- cmake/torch.cmake
+++ cmake/torch.cmake
@@ -1,23 +1,30 @@
-if(APPLE)
-    message(STATUS "Getting Torch: Apple CPU")
-    FetchContent_Declare(
-        Torch
-        URL https://download.pytorch.org/libtorch/nightly/cpu/libtorch-macos-latest.zip
-    )
+# check if torch can be found on the system default
+find_package(Torch QUIET)
+
+# if not found download a version and populate it
+if(Torch_FOUND)
+    message(STATUS "Found Installed Torch: ${TORCH_INCLUDE_DIRS}")
 else()
-    message(STATUS "Getting Torch: Linux CPU")
-    FetchContent_Declare(
-        Torch
-        URL https://download.pytorch.org/libtorch/nightly/cpu/libtorch-cxx11-abi-shared-with-deps-latest.zip
-    )
+    if(APPLE)
+        message(STATUS "Getting Torch: Apple CPU")
+        FetchContent_Declare(
+            Torch
+            URL https://download.pytorch.org/libtorch/nightly/cpu/libtorch-macos-latest.zip
+        )
+    else()
+        message(STATUS "Getting Torch: Linux CPU")
+        FetchContent_Declare(
+            Torch
+            URL https://download.pytorch.org/libtorch/nightly/cpu/libtorch-cxx11-abi-shared-with-deps-latest.zip
+        )
+    endif()
+    
+    FetchContent_MakeAvailable(Torch)
+    
+    set(Torch_DIR "${FETCHCONTENT_BASE_DIR}/torch-src/share/cmake/Torch")
+    
+    find_package(Torch REQUIRED)
 endif()
-
-
-FetchContent_MakeAvailable(Torch)
-
-set(Torch_DIR "${FETCHCONTENT_BASE_DIR}/torch-src/share/cmake/Torch")
-
-find_package(Torch REQUIRED)
 
 message(STATUS "Torch dir: ${Torch_DIR}")
 message(STATUS "Adding TORCH_CXX_FLAGS: ${TORCH_CXX_FLAGS}")
