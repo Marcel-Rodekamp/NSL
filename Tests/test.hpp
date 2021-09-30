e777cd5564304e528e219115727291c87327ce3d
Evan Berkowitz <git@evanberkowitz.com>
2021-09-30 20:01:55 +0200

    Leverage catch2 feature: TEMPLATE_TEST_CASE
    According to [the catch2 documenation][catch2-docs],
    `TEMPLATE_TEST_CASE` is the same as `TEST_CASE` except the
    all the arguments after the second are types which are
    looped over, giving different test cases.
    
    In "Tests/test.hpp" I include catch2 and then provide macros which
    expand to sequences of types: `REAL_TYPES`, `COMPLEX_TYPES` and
    `FLOATING_POINT_TYPES` for floats, and `TORCH_TYPES` to include `int` and
    `bool`.
    
    Finally, I implemente a `TORCH_TYPE_TEST_CASE` macro which expands to a
    catch2-native `TEMPLATE_TEST_CASE` with the appropriate types inserted.
    
    In test_tensor.cpp I replaced three very verbose examples which had
    explicit instantiation for each type with different, equivalent test
    cases.  Two,
     - `"TENSOR: 2D Random access"`
     - `"TENSOR: 3D Random access"`
    showing how to use the catch2 `TEMPLATE_TEST_CASE` explicitly and one
     - `"TENSOR: 1D Random access"`
    which uses the custom macro.
    
    The tests compile and pass.
    
    [catch2-docs]: https://github.com/catchorg/Catch2/blob/devel/docs/test-cases-and-sections.md#type-parametrised-test-cases

---
+++ Tests/test.hpp
@@ -0,0 +1,24 @@
+#ifndef NSL_TEST_HPP
+#define NSL_TEST_HPP
+
+#include "catch2/catch.hpp"
+
+
+// ====================================================================
+// Type macros
+// ====================================================================
+
+#define REAL_TYPES float, double
+#define COMPLEX_TYPES NSL::complex<float>, NSL::complex<double>
+#define FLOATING_POINT_TYPES REAL_TYPES, COMPLEX_TYPES
+
+#define TORCH_TYPES int, FLOATING_POINT_TYPES, bool
+
+// ====================================================================
+// Custom TEST_CASE macros
+// ====================================================================
+
+#define TORCH_TYPE_TEST_CASE(_1, _2) TEMPLATE_TEST_CASE(_1, _2, TORCH_TYPES)
+
+
+#endif
