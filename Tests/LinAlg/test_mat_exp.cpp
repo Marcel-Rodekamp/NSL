d0ffa552885c3111e6ded2ef00a5414c13065b91
Marcel Rodekamp <marcel.rodekamp@gmail.com>
2021-10-20 17:39:40 +0200

    Added mat_mul to includes

--- Tests/LinAlg/test_mat_exp.cpp
+++ Tests/LinAlg/test_mat_exp.cpp
@@ -2,6 +2,7 @@
 #include "complex.hpp"
 #include "../test.hpp"
 #include "LinAlg/mat_exp.hpp"
+#include "LinAlg/mat_mul.hpp"
 #include "LinAlg/exp.hpp"
 #include "LinAlg/det.hpp"
 #include "LinAlg/abs.hpp"
