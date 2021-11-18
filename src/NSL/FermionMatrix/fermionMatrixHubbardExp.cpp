436a51004255b95b85878c42f0dcba0caa280874
keshvituteja <52911730+keshvituteja@users.noreply.github.com>
2021-11-18 10:48:26 +0100

    Implemetation of M, Mdagger, MdaggerM (boundary condition missing)

--- src/NSL/FermionMatrix/fermionMatrixHubbardExp.cpp
+++ src/NSL/FermionMatrix/fermionMatrixHubbardExp.cpp
@@ -15,18 +15,18 @@ NSL::TimeTensor<Type> NSL::FermionMatrix::FermionMatrixHubbardExp<Type>::F_(cons
     // apply kronecker delta
     NSL::TimeTensor<Type> psiShift = NSL::LinAlg::shift(psi,1);
 
-    NSL::TimeTensor<Type> Fpsi = NSL::LinAlg::mat_vec(
+    NSL::TimeTensor<Type> Fpsi = (NSL::LinAlg::mat_vec(
         //! \todo: This argument needs to be variable with the beta coming from elsewhere
         //!        CHANGE THAT!!!
         // exp_hopping_matrix computes only once and stores the result accessible with the same function
         this->Lat->exp_hopping_matrix(/*delta=(beta/Nt) */0.1),
-        ((this->phi_*I).exp() * psiShift).transpose()
+        ((this->phi_*I).exp() * psiShift).transpose()).transpose()
     );
 
     // anti-periodic boundary condition
 
     Fpsi.slice(0,0,1)*=-1;
-    return Fpsi.transpose();
+    return Fpsi;
 }
 
 template <typename Type>
@@ -36,21 +36,49 @@ NSL::TimeTensor<Type> NSL::FermionMatrix::FermionMatrixHubbardExp<Type>::M(const
 
 template<typename Type>
 NSL::TimeTensor<Type> NSL::FermionMatrix::FermionMatrixHubbardExp<Type>::Mdagger(const NSL::TimeTensor<Type> & psi){
-    //NSL::TimeTensor<Type> out;
-    //out= this->M(psi);    
-    return (NSL::LinAlg::adjoint(this->M(psi)));
+    NSL::TimeTensor<Type> out;
+    const std::size_t Nt = this->phi_.shape(0);
+    const std::size_t Nx = this->phi_.shape(1);
+
+    const NSL::complex<typename RT_extractor<Type>::value_type> I(0,1);
+
+    NSL::TimeTensor<Type> psiShift = NSL::LinAlg::shift(psi,1);
+    out= NSL::LinAlg::mat_vec(NSL::LinAlg::adjoint((this->phi_*I).exp()).transpose(), this->Lat->exp_hopping_matrix(0.1)) * psiShift;
+    return (psi-out);
 }
 
 template<typename Type>
 NSL::TimeTensor<Type> NSL::FermionMatrix::FermionMatrixHubbardExp<Type>::MMdagger(const NSL::TimeTensor<Type> & psi){
-   
-    return (NSL::LinAlg::mat_mul(this->M(psi),this->Mdagger(psi)));
+    NSL::TimeTensor<Type> out;
+    const std::size_t Nt = this->phi_.shape(0);
+    const std::size_t Nx = this->phi_.shape(1);
+
+    const NSL::complex<typename RT_extractor<Type>::value_type> I(0,1);
+
+    NSL::TimeTensor<Type> psiShift = NSL::LinAlg::shift(psi,1);
+    NSL::TimeTensor<Type> phi = this->phi_;
+    out= (NSL::LinAlg::mat_vec(this->Lat->exp_hopping_matrix(0.1),((phi*I).exp()).transpose())* NSL::LinAlg::adjoint((this->phi_*I).exp())).transpose();
+    out=  NSL::LinAlg::mat_vec(out, this->Lat->exp_hopping_matrix(0.1)) * psiShift;
+    return (psi+out);
 }
 
 template<typename Type>
 NSL::TimeTensor<Type> NSL::FermionMatrix::FermionMatrixHubbardExp<Type>::MdaggerM(const NSL::TimeTensor<Type> & psi){
-   
-    return (NSL::LinAlg::mat_mul(this->Mdagger(psi),this->M(psi)));
+    NSL::TimeTensor<Type> out;
+    const std::size_t Nt = this->phi_.shape(0);
+    const std::size_t Nx = this->phi_.shape(1);
+
+    const NSL::complex<typename RT_extractor<Type>::value_type> I(0,1);
+
+    NSL::TimeTensor<Type> psiShift = NSL::LinAlg::shift(psi,1);
+
+    //Since, phi.exp() changes phi_, copy it in phi because there are two exponential terms
+
+    NSL::TimeTensor<Type> phi = this->phi_;
+    out=  NSL::LinAlg::mat_vec(NSL::LinAlg::adjoint((phi*I).exp()).transpose(), this->Lat->exp_hopping_matrix(0.1));
+    out=  NSL::LinAlg::mat_vec(out, this->Lat->exp_hopping_matrix(0.1)) * ((this->phi_*I).exp());
+    out= out * psiShift;
+    return (psi+out);
 }
 
 //! \todo: Full support of complex number multiplication is missing in Tensor:
