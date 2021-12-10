725294a127df03f1f5cbab4ce730902d81aad39d
keshvituteja <52911730+keshvituteja@users.noreply.github.com>
2021-12-10 15:34:24 +0100

    to be used for benchmarking

---
+++ Executables/Examples/example_callM.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "complex.hpp"
+#include "Tensor/tensor.hpp"
+#include "Lattice/Implementations/ring.hpp"
+#include "FermionMatrix/fermionMatrixHubbardExp.hpp"
+#include "Linalg/mat_conj.hpp"
+#include "Linalg/mat_exp.hpp"
+
+int main(){
+
+    //Initializing
+    NSL::TimeTensor<NSL::complex<double>> phi(2,2);
+    NSL::TimeTensor<NSL::complex<double>> psi(2,2);
+    phi.rand();
+    psi.rand();
+    NSL::complex<double> iota = {0,1};
+    double min_one =-1;    
+    NSL::Lattice::Ring<double> r(2); 
+
+    //calling the function M
+    NSL::FermionMatrix::FermionMatrixHubbardExp<NSL::complex<double>> M(&r,psi);
+
+
+    return EXIT_SUCCESS;
+}
