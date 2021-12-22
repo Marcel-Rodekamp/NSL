6cd9c8bbb5c3982cadb9cb2dc30573b4fcc2248f
Marcel Rodekamp <32614876+Marcel-Rodekamp@users.noreply.github.com>
2021-12-22 19:05:55 +0100

    Cmake update (#24)
    * Removed old kokkos
    
    * Romoved old kokkos add on
    
    * Made TEST and BENCHMARK to a set of executables not a stand alone library
    
    * Removed Double Compiling: target_sources, PUBLIC -> PRIVATE
    
    * Added testing suit, tests can now be executed as 'make test' or using each executable individually

--- Tests/test_main.cpp
+++ Tests/test_main.cpp
@@ -1,14 +1,3 @@
 #define CATCH_CONFIG_MAIN
 #include "test.hpp"
-/*#include <Kokkos_Core.hpp>*/
 
-/*
-int main( int argc, char* argv[] ) {
-    // ToDo: We should hide Kokkos::initialization in a NSL::initialization()!
-    Kokkos::initialize(argc, argv);
-
-    int result = Catch::Session().run( argc, argv );
-
-    Kokkos::finalize();
-    return result;
-}*/
