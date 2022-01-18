dc5c4fc61839aa1b8b76016dac55bafa20b1464f
Marcel Rodekamp <marcel.rodekamp@gmail.com>
2022-01-18 13:41:10 +0100

    Fixed compilation (redefinition) issue as TimeTensor and Tensor are the same

--- src/NSL/LinAlg/mat_vec.hpp
+++ src/NSL/LinAlg/mat_vec.hpp
@@ -20,27 +20,6 @@ namespace NSL{
             return aux;
         }
 
-        //Tensor x TimeTensor
-        template<typename Type>
-        NSL::TimeTensor<Type> mat_vec(const NSL::Tensor<Type> & matrix, const NSL::TimeTensor<Type> & vector){
-            NSL::Tensor<Type> aux(torch::matmul(to_torch(matrix), to_torch(vector)));
-            return aux;
-        }
-
-        //TimeTensor x TimeTensor
-        template<typename Type>
-        NSL::TimeTensor<Type> mat_vec( const NSL::TimeTensor<Type> & matrix, const NSL::TimeTensor<Type> & vector){
-            NSL::Tensor<Type> aux(torch::matmul(to_torch(matrix), to_torch(vector)));
-            return aux;
-        }
-
-        //TimeTensor x Tensor
-        template<typename Type>
-        NSL::TimeTensor<Type> mat_vec(const NSL::TimeTensor<Type> & matrix, const NSL::Tensor<Type> & vector){
-            NSL::Tensor<Type> aux(torch::matmul(to_torch(matrix), to_torch(vector)));
-            return aux;
-        }
-
         // =====================================================================
         // Expansion
         // =====================================================================
@@ -52,15 +31,6 @@ namespace NSL{
             aux.copy(tensor);
             aux.expand(dims);
             return aux;
-        }
-
-        //Expansion of a TimeTensor
-        template<typename Type>
-        NSL:: TimeTensor<Type> expand(const TimeTensor<Type> & tensor, std::deque<long int> & dims){
-                NSL::TimeTensor<Type> aux;
-                aux.copy(tensor);
-                aux.expand(dims);
-                return aux;
         }
 
         // =====================================================================
@@ -80,24 +50,6 @@ namespace NSL{
         template<typename Type>
         NSL::Tensor<Type>  shift( const NSL::Tensor<Type> & tensor, const long int & shift){
             NSL::Tensor<Type> aux;
-            aux.copy(tensor);
-            aux.shift(shift);
-            return aux;
-        }
-
-        //Shift TimeTensor
-        template<typename Type>
-        NSL::TimeTensor<Type>  shift(const  NSL::TimeTensor<Type> & tensor, const long int & shift, const Type & boundary){
-            NSL::TimeTensor<Type> aux;
-            aux.copy(tensor);
-            aux.shift(shift, boundary);
-            return aux;
-        }
-
-        //Shift TimeTensor
-        template<typename Type>
-        NSL::TimeTensor<Type>  shift( NSL::TimeTensor<Type> & tensor, const long int & shift){
-            NSL::TimeTensor<Type> aux;
             aux.copy(tensor);
             aux.shift(shift);
             return aux;
