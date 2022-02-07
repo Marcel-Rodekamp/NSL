e1136c80af2f523ecf6bf3b0656b7e5dab5be1a2
keshvituteja <52911730+keshvituteja@users.noreply.github.com>
2022-02-07 11:55:48 +0100

    Matrix class

---
+++ src/NSL/LinAlg/matrix.hpp
@@ -0,0 +1,21 @@
+#ifndef NSL_LINALG_MATRIX_HPP
+#define NSL_LINALG__MATRIX_HPP
+
+#include "../Tensor/tensor.hpp"
+
+namespace NSL {
+namespace LinAlg {
+namespace Matrix{
+
+template <typename Type> 
+    NSL::TimeTensor<Type> Identity(const size_t & size, NSL::TimeTensor<Type> & t ) {
+        NSL::TimeTensor<Type> out(torch::eye(size, torch::TensorOptions().dtype<Type>()));
+        return (out);
+    }
+
+
+} //namespace Matrix
+} // namespace LinAlg
+} // namespace NSL
+
+#endif
