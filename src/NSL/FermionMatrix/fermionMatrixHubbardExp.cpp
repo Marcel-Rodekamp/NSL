fe9cf7188f0ef50c83299e577bf0ceae01216f10
keshvituteja <52911730+keshvituteja@users.noreply.github.com>
2022-02-07 11:53:09 +0100

    Implementation of M(psi), Mdagger(psi), MMdagger(psi), MdaggerM(psi), logDetM() and logDetMdagger()

--- src/NSL/FermionMatrix/fermionMatrixHubbardExp.cpp
+++ src/NSL/FermionMatrix/fermionMatrixHubbardExp.cpp
@@ -9,7 +9,7 @@ NSL::TimeTensor<Type> NSL::FermionMatrix::FermionMatrixHubbardExp<Type>::F_(cons
     const std::size_t Nx = this->phi_.shape(1);
 
     //! \todo Add complex literals
-    const NSL::complex<typename RT_extractor<Type>::value_type> I(0,1);
+    //const NSL::complex<typename RT_extractor<Type>::value_type> I(0,1);
 
     // apply kronecker delta
     NSL::TimeTensor<Type> psiShift = NSL::LinAlg::shift(psi,1);
@@ -38,9 +38,11 @@ NSL::TimeTensor<Type> NSL::FermionMatrix::FermionMatrixHubbardExp<Type>::Mdagger
     NSL::TimeTensor<Type> out;
     const std::size_t Nt = this->phi_.shape(0);
     const std::size_t Nx = this->phi_.shape(1);
-    const NSL::complex<typename RT_extractor<Type>::value_type> I(0,1);
+    //const NSL::complex<typename RT_extractor<Type>::value_type> I(0,1);
 
+    // apply kronecker delta
     NSL::TimeTensor<Type> psiShift = NSL::LinAlg::shift(psi,1);
+
     out= NSL::LinAlg::mat_vec(
         NSL::LinAlg::adjoint(this->phiExp_).transpose(),
          this->Lat->exp_hopping_matrix(0.1))
@@ -55,7 +57,7 @@ NSL::TimeTensor<Type> NSL::FermionMatrix::FermionMatrixHubbardExp<Type>::MMdagge
     NSL::TimeTensor<Type> out;
     const std::size_t Nt = this->phi_.shape(0);
     const std::size_t Nx = this->phi_.shape(1);
-    const NSL::complex<typename RT_extractor<Type>::value_type> I(0,1);
+    //const NSL::complex<typename RT_extractor<Type>::value_type> I(0,1);
 
     out= this->M(psi) + this->Mdagger(psi) + NSL::LinAlg::mat_vec((this->Lat->exp_hopping_matrix(0.1))*
         (this->Lat->exp_hopping_matrix(0.1)), NSL::LinAlg::mat_transpose(psi)).
@@ -70,13 +72,13 @@ NSL::TimeTensor<Type> NSL::FermionMatrix::FermionMatrixHubbardExp<Type>::Mdagger
     NSL::TimeTensor<Type> out;
     const std::size_t Nt = this->phi_.shape(0);
     const std::size_t Nx = this->phi_.shape(1);
-    const NSL::complex<typename RT_extractor<Type>::value_type> I(0,1);
+    //const NSL::complex<typename RT_extractor<Type>::value_type> I(0,1);
 
 
     out= this->M(psi) + this->Mdagger(psi) + (NSL::LinAlg::adjoint(this->phiExp_) *
         NSL::LinAlg::mat_vec((this->Lat->exp_hopping_matrix(0.1))*
          (this->Lat->exp_hopping_matrix(0.1)),
-        (NSL::LinAlg::mat_transpose(this->phiExp_)))).transpose();
+        ((this->phiExp_ * psi).transpose()))).transpose();
 
     return (out-psi);
 
@@ -96,14 +98,16 @@ Type NSL::FermionMatrix::FermionMatrixHubbardExp<Type>::logDetM(){
     NSL::TimeTensor<Type> prod(Nt,Nx,Nx);
     NSL::TimeTensor<Type> out(Nx,Nx);
     Type logdet = 0.0;
-    //F_{N_t-1}
-    prod = this->Lat->exp_hopping_matrix(0.1)* NSL::LinAlg::shift(this->phiExp_,-1).expand(Nx).transpose(1,2);
     
+    prod = this->Lat->exp_hopping_matrix(0.1)* NSL::LinAlg::shift(this->phiExp_,-1).expand(Nx).transpose(1,2);
+    //F_{N_t-1}
     out = prod.slice(/*dim=*/0,/*start=*/Nt-1,/*end=*/Nt);
     
     for(int t=Nt-2; t>=0; t--){
-        //out = NSL::LinAlg::mat_vec(out,prod.slice(/*dim=*/0,/*start=*/t,/*end=*/t+1));
-        out.mat_mul(prod.slice(/*dim=*/0,/*start=*/t,/*end=*/t+1)); 
+
+        //! \todo: figure out what mat_mul does
+        out = NSL::LinAlg::mat_vec(out,prod.slice(/*dim=*/0,/*start=*/t,/*end=*/t+1));
+        //out.mat_mul(prod.slice(/*dim=*/0,/*start=*/t,/*end=*/t+1)); 
          
     }
       
@@ -130,58 +134,37 @@ Type NSL::FermionMatrix::FermionMatrixHubbardExp<Type>::logDetMdagger() {
     NSL::TimeTensor<Type> prod;
     NSL::TimeTensor<Type> Ainv;
     Type logdet(0,0), phiSum(0,0);
-    //F_{N_t-1}
 
-//    prod = this->Lat->exp_hopping_matrix(0.1)* NSL::LinAlg::shift(this->phiExp_,-1).expand(Nx).transpose(1,2);
-//    Ainv = NSL::LinAlg::mat_inv(prod.slice(/*dim=*/0,/*start=*/0,/*end=*/1));
-//    for(int i=1; i<Nt; i--){
-//        Ainv = Ainv * NSL::LinAlg::mat_inv(prod.slice(/*dim=*/0,/*start=*/i,/*end=*/i));
+
+    prod = this->Lat->exp_hopping_matrix(0.1)* NSL::LinAlg::shift(this->phiExp_,-1).expand(Nx).transpose(1,2);
+    //F_{N_0}
+    Ainv = NSL::LinAlg::mat_inv(prod.slice(/*dim=*/0,/*start=*/0,/*end=*/1));
+    for(int t=1; t<Nt; t++){
+        Ainv *= NSL::LinAlg::mat_inv(prod.slice(/*dim=*/0,/*start=*/t,/*end=*/t+1));
         
-//    }
+    }
 
 
-//    for(int j=0; j<Nt; j++){
-//        for(int k=0; k<Nx; k++){
-//            phiSum=phiSum + this->phi_(j,k); //check if it works
-//            }
-//    }
-//    logdet = NSL::LinAlg::logdet(Ainv + Id.Identity(Nx)) - 
-//               (phiSum*I) -Nt*NSL::LinAlg::logdet(this->Lat->exp_hopping_matrix(0.1)); //confirm sign of hopping term
-                
+    for(int j=0; j<Nt; j++){
+        for(int k=0; k<Nx; k++){
+            phiSum += this->phi_(j,k); //check if it works
+            }
+    }
+    //! \todo: confirm how to do this!
+    if(this->sigma_==1) {
+        logdet = NSL::LinAlg::logdet(Ainv + Id.Identity(Nx)) - 
+                   (phiSum*I) -Nt*NSL::LinAlg::logdet(this->Lat->exp_hopping_matrix(0.1)); //confirm sign of hopping term
+        }
+    else  {
+        logdet = NSL::LinAlg::logdet(Ainv + Id.Identity(Nx)) - 
+               (phiSum*I) -Nt*NSL::LinAlg::logdet(this->Lat->exp_hopping_matrix(-0.1)); //confirm sign of hopping term
+         
+}            
 
     return logdet;
    
 }
 
-/*
-template<typename Type>
-NSL::TimeTensor<Type> NSL::FermionMatrix::FermionMatrixHubbardExp<Type>::F(){
-    NSL::TimeTensor<Type> out;
-    const NSL::complex<typename RT_extractor<Type>::value_type> I(0,1);
-    NSL::TimeTensor<Type> phiShift = NSL::LinAlg::shift(this->phi_,-1);
-
-    out=NSL::LinAlg::mat_vec(this->Lat->exp_hopping_matrix(0.1),
-        NSL::LinAlg::mat_transpose(NSL::LinAlg::exp(phi*I)));
-
-    return(out);
-}
-*/
-/*
-template<typename Type>
-NSL::complex<Type> NSL::FermionMatrix::FermionMatrixHubbardExp<Type>::logDetM(const NSL::TimeTensor<Type> & psi){
-    const std::size_t Nt = this->phi_.shape(0);
-    const std::size_t Nx = this->phi_.shape(1);  
-    
-    NSL::TimeTensor<Type> prod;
-    NSL::complex<Type> out;
-    //F_{N_t-1}
-    prod = this->Lat->exp_hopping_matrix(0.1)* NSL::LinAlg::shift(this->phiExp_,-(1))
-    for(int i=0; i<Nt-2; i++){
-        
-    }
-
-}
-*/
 //! \todo: Full support of complex number multiplication is missing in Tensor:
 //template class NSL::FermionMatrix::FermionMatrixHubbardExp<float>;
 //template class NSL::FermionMatrix::FermionMatrixHubbardExp<double>;
