c1b31607eeabef838f7f9ed1caa2251c64288650
keshvituteja <52911730+keshvituteja@users.noreply.github.com>
2022-02-12 14:29:03 +0100

    removed logDetMdagger()

--- src/NSL/FermionMatrix/fermionMatrixHubbardExp.hpp
+++ src/NSL/FermionMatrix/fermionMatrixHubbardExp.hpp
@@ -3,14 +3,15 @@
 
 #include "LinAlg/mat_vec.hpp"
 #include "fermionMatrixBase.hpp"
-#include "../LinAlg/mat_conj.hpp"
-#include "../LinAlg/mat_mul.hpp"
-#include "../LinAlg/mat_exp.hpp"
-#include "../LinAlg/mat_trans.hpp"
+#include "LinAlg/mat_conj.hpp"
+#include "LinAlg/mat_mul.hpp"
+#include "LinAlg/mat_exp.hpp"
+#include "LinAlg/mat_trans.hpp"
 #include "LinAlg/mat_inv.hpp"
-#include "../LinAlg/det.hpp"
-#include "../LinAlg/exp.hpp"
-#include "../Tensor/Matrices/matricesBase.hpp"
+#include "LinAlg/det.hpp"
+#include "LinAlg/exp.hpp"
+#include "LinAlg/matrix.hpp"
+
 
 namespace NSL::FermionMatrix {
 template<typename Type>
@@ -27,9 +28,10 @@ class FermionMatrixHubbardExp : public FermionMatrixBase<Type> {
      *
      * \todo: SpatialLattice accepts only real type template arguments
      * */
-    FermionMatrixHubbardExp(NSL::Lattice::SpatialLattice<typename NSL::RT_extractor<Type>::value_type> *lat, const NSL::TimeTensor<Type> &phi):
+    FermionMatrixHubbardExp(NSL::Lattice::SpatialLattice<typename NSL::RT_extractor<Type>::value_type> *lat,  const NSL::TimeTensor<Type> &phi, double beta = 1.0 ):
         FermionMatrixBase<Type>(lat),
         phi_(phi),
+        delta_(beta/phi.shape(0)),
         phiExp_(NSL::LinAlg::exp(phi*NSL::complex<typename NSL::RT_extractor<Type>::value_type>(0,1)))
         
 
@@ -40,14 +42,13 @@ class FermionMatrixHubbardExp : public FermionMatrixBase<Type> {
     NSL::TimeTensor<Type> MMdagger(const NSL::TimeTensor<Type> & psi) override;
     NSL::TimeTensor<Type> MdaggerM(const NSL::TimeTensor<Type> & psi) override;
     Type logDetM() override;
-    Type logDetMdagger() override;
     /*NSL::complex<Type> logDetM(const NSL::TimeTensor<Type> & psi) override;
     NSL::TimeTensor<Type> NSL::FermionMatrix::FermionMatrixHubbardExp<Type>::F() override;
     */
     protected:
     NSL::Tensor<Type> phi_;
     NSL::Tensor<Type> phiExp_;
-    static constexpr double sigma_=1.0;
+    double delta_;
 
     private:
     NSL::TimeTensor<Type> F_(const NSL::TimeTensor<Type> & psi);
