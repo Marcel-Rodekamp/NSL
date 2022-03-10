177981613ea75a030f60e2dfe10e211df89642cc
keshvituteja <52911730+keshvituteja@users.noreply.github.com>
2022-03-10 19:06:11 +0100

    cleaning up

--- src/NSL/FermionMatrix/fermionMatrixBase.hpp
+++ src/NSL/FermionMatrix/fermionMatrixBase.hpp
@@ -1,6 +1,14 @@
 #ifndef NSL_FERM_BASE_HPP
 #define NSL_FERM_BASE_HPP
 
+/*! \file fermionMatrixBase.hpp
+ *  Class for exponential discretization of fermion matrix.
+ *
+ *  The class contains methods which would be used for
+ *  the computation of fermionic action
+ *    
+ **/
+
 #include<vector>
 #include "../Lattice/lattice.hpp"
 #include "../assert.hpp"
@@ -12,6 +20,10 @@ namespace NSL::FermionMatrix{
 
 //definition of class FermionMatrixBase
 
+/*! A base class for exponential dizcreatization of the fermion
+*   matrix
+**/
+
 template<typename Type>
 class FermionMatrixBase {
 
@@ -19,52 +31,51 @@ class FermionMatrixBase {
 
     //Declaration of methods methods M, M_dagger, MM_dagger and M
 
+    /*!
+    *  \param psi a vector with the dimensions N_t x N_x.
+    *  \returns M acting on psi, M.psi.
+    **/
     virtual NSL::TimeTensor<Type> M(const NSL::TimeTensor<Type> & psi) = 0;
-    virtual NSL::TimeTensor<Type> Mdagger(const NSL::TimeTensor<Type> & psi) = 0;
-    virtual NSL::TimeTensor<Type> MMdagger(const NSL::TimeTensor<Type> & psi) = 0;
-    virtual NSL::TimeTensor<Type> MdaggerM(const NSL::TimeTensor<Type> & psi) = 0;
-    virtual Type logDetM() = 0;
-//    virtual NSL::TimeTensor<Type> Mdagger(const NSL::TimeTensor<Type> & psi);
-//    virtual NSL::TimeTensor<Type> MMdagger(const NSL::TimeTensor<Type> & psi);
-//    virtual NSL::TimeTensor<Type> MdaggerM(const NSL::TimeTensor<Type> & psi);
 
-//    NSL::complex<NSL::RT_extractor<Type>::value_type> detM();
-//    NSL::complex<NSL::RT_extractor<Type>::value_type> detMdagger() {
-//        return NSL::conj(this->detM());
-//    }
-//    NSL::complex<NSL::RT_extractor<Type>::value_type> detMdaggerM();
-//    NSL::complex<NSL::RT_extractor<Type>::value_type> detMMdagger(){
-//        return this->detMdaggerM();
-//    }
-//
-//    NSL::complex<NSL::RT_extractor<Type>::value_type> logdetM();
-//    NSL::complex<NSL::RT_extractor<Type>::value_type> logdetMdagger() {
-//        //! \todo: How to do log as in detMdagger
-//        //!        return NSL::conj(this->detM());
-//        return static_cast<NSL::complex<RT_extractor<Type>::value_type>>(0);
-//    }
-//    NSL::complex<NSL::RT_extractor<Type>::value_type> logdetMdaggerM();
-//    NSL::complex<NSL::RT_extractor<Type>::value_type> logdetMMdagger(){
-//        //! \todo: How to do log as in detMdaggerM
-//        //!        return this->detMdaggerM();
-//        return static_cast<NSL::complex<RT_extractor<Type>::value_type>>(0);
-//    }
+    /*!
+    *  \param psi a vector with the dimensions N_t x N_x.
+    *  \returns Mdagger acting on psi, Mdagger.psi.
+    **/
+    virtual NSL::TimeTensor<Type> Mdagger(const NSL::TimeTensor<Type> & psi) = 0;
+
+    /*!
+    *  \param psi a vector with the dimensions N_t x N_x.
+    *  \returns MMdagger acting on psi, MMdagger.psi.
+    **/
+    virtual NSL::TimeTensor<Type> MMdagger(const NSL::TimeTensor<Type> & psi) = 0;
+
+    /*!
+    *  \param psi a vector with the dimensions N_t x N_x.
+    *  \returns MdaggerM acting on psi, MdaggerM.psi.
+    **/
+    virtual NSL::TimeTensor<Type> MdaggerM(const NSL::TimeTensor<Type> & psi) = 0;
+
+    /*!
+    *  \returns log of determinant of M.
+    **/
+    virtual Type logDetM() = 0;
 
     // constructors
+    /*  There is no default constructor. */
     FermionMatrixBase() = delete;
     FermionMatrixBase(FermionMatrixBase<Type> &) = delete;
     FermionMatrixBase(FermionMatrixBase<Type> &&) = delete;
-
+    /*! 
+    *  \param lat  an object of Lattice type (Ring, square, etc.).
+    **/
     FermionMatrixBase(NSL::Lattice::SpatialLattice<typename NSL::RT_extractor<Type>::value_type> * lat):
         Lat(lat)
     {}
 
-
     protected:
-
+    //! An object of Lattice type (Ring, square, etc.).
     NSL::Lattice::SpatialLattice<typename NSL::RT_extractor<Type>::value_type>* Lat;
 };
-
 }
 
 #endif
