4bda0f408c1d07f5e9b5d671239940cff893ef00
keshvituteja <52911730+keshvituteja@users.noreply.github.com>
2022-03-10 11:29:43 +0100

    calls the functions in fermionMatrixHubbardExp.cpp

--- Executables/Examples/example_fermionMatrixHubbardExp.cpp
+++ Executables/Examples/example_fermionMatrixHubbardExp.cpp
@@ -3,21 +3,30 @@
 #include "Tensor/tensor.hpp"
 #include "Lattice/Implementations/ring.hpp"
 #include "FermionMatrix/fermionMatrixHubbardExp.hpp"
+#include "LinAlg/mat_conj.hpp"
+#include "LinAlg/mat_exp.hpp"
+#include <fstream>
 
+
+#include "LinAlg/mat_vec.hpp"
+#include "LinAlg/mat_mul.hpp"
+#include "LinAlg/mat_trans.hpp"
+#include "LinAlg/det.hpp"
+#include "LinAlg/mat_inv.hpp"
+#include "math.h"
 
 
 
 int main(){
-    NSL::TimeTensor<NSL::complex<double>> phi(16,2);
-    NSL::TimeTensor<NSL::complex<double>> psi(16,2);
-
+    NSL::TimeTensor<NSL::complex<double>> phi(16,2), psi(16,2);
     phi.rand();
     psi.rand();
+    NSL::Lattice::Ring<double> ring(2);
+    double beta=0.1;
+    double delta = beta/100;
+
+    NSL::FermionMatrix::FermionMatrixHubbardExp<NSL::complex<double>> M(&ring,phi,beta);
     
-
-    NSL::Lattice::Ring<double> r(2); 
-    NSL::FermionMatrix::FermionMatrixHubbardExp<NSL::complex<double>> M(&r,psi);
-
     std::cout<<M.M(psi).real()<<std::endl;
     std::cout<<M.M(psi).imag()<<std::endl; 
 
@@ -32,12 +41,6 @@ int main(){
     std::cout<<M.Mdagger(psi).imag()<<std::endl;
 
     std::cout<<M.logDetM()<<std::endl;
-
-    std::cout<<M.logDetMdagger()<<std::endl;
-
-
-   
-
 
     return EXIT_SUCCESS;
 }
