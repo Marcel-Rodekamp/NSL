01a4de8829ef68b98a9c6851128b180166ce3f9c
keshvituteja <52911730+keshvituteja@users.noreply.github.com>
2022-03-10 19:06:22 +0100

    cleaning up

--- src/NSL/FermionMatrix/fermionMatrixHubbardExp.hpp
+++ src/NSL/FermionMatrix/fermionMatrixHubbardExp.hpp
@@ -1,6 +1,14 @@
 #ifndef NANOSYSTEMLIBRARY_FERMIONMATRIXHUBBARDEXP_HPP
 #define NANOSYSTEMLIBRARY_FERMIONMATRIXHUBBARDEXP_HPP
 
+/*! \file fermionMatrixHubbardExp.hpp
+ *  Class for exponential discretization of fermion matrix.
+ *
+ *  The class contains methods which would be used for
+ *  the computation of fermionic action
+ *    
+ **/
+
 #include "LinAlg/mat_vec.hpp"
 #include "fermionMatrixBase.hpp"
 #include "LinAlg/mat_conj.hpp"
@@ -21,13 +29,21 @@ template<typename Type>
 class FermionMatrixHubbardExp : public FermionMatrixBase<Type> {
 
     public:
+        //  No default constructor
+        /*  There is no default constructor. */
     FermionMatrixHubbardExp() = delete;
 
     //!
     /*!
      *
      * \todo: SpatialLattice accepts only real type template arguments
-     * */
+     *   
+    *! 
+    *  \param lat  an object of lattice type (Ring, square, etc.).
+    *  \param phi  a tensor with dimensions N_t x N_x
+    *  \param beta  a floating point number where delta=beta/N_t.
+    **/
+    
     FermionMatrixHubbardExp(NSL::Lattice::SpatialLattice<typename NSL::RT_extractor<Type>::value_type> *lat,  const NSL::TimeTensor<Type> &phi, double beta = 1.0 ):
         FermionMatrixBase<Type>(lat),
         phi_(phi),
@@ -37,15 +53,43 @@ class FermionMatrixHubbardExp : public FermionMatrixBase<Type> {
 
     {}
 
+    //Declaration of methods methods M, M_dagger, MM_dagger and M
+
+    /*!
+    *  \param psi a vector with the dimensions N_t x N_x.
+    *  \returns M acting on psi, M.psi.
+    **/
     NSL::TimeTensor<Type> M(const NSL::TimeTensor<Type> & psi) override;
+
+    /*!
+    *  \param psi a vector with the dimensions N_t x N_x.
+    *  \returns Mdagger acting on psi, Mdagger.psi.
+    **/
     NSL::TimeTensor<Type> Mdagger(const NSL::TimeTensor<Type> & psi) override;
+
+    /*!
+    *  \param psi a vector with the dimensions N_t x N_x.
+    *  \returns MMdagger acting on psi, MMdagger.psi.
+    **/
     NSL::TimeTensor<Type> MMdagger(const NSL::TimeTensor<Type> & psi) override;
+
+    /*!
+    *  \param psi a vector with the dimensions N_t x N_x.
+    *  \returns MdaggerM acting on psi, MdaggerM.psi.
+    **/
     NSL::TimeTensor<Type> MdaggerM(const NSL::TimeTensor<Type> & psi) override;
+
+    /*!
+    *  \returns log of determinant of M.
+    **/
     Type logDetM() override;
 
     protected:
+    //! The configuration phi (N_t x N_x)
     NSL::Tensor<Type> phi_;
+    //! Exponential of phi
     NSL::Tensor<Type> phiExp_;
+    //! delta = beta/N_t
     NSL::RT_extractor<Type>::value_type delta_;
 
     private:
