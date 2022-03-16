59d16ef84244ec7ce9df115685c2a9df665250ac
Marcel Rodekamp <marcel.rodekamp@gmail.com>
2022-03-16 16:07:45 +0100

    Moved LinAlg/*.hpp -> LinAlg/*.tpp

--- src/NSL/LinAlg.hpp
+++ src/NSL/LinAlg.hpp
@@ -4,11 +4,11 @@
 #include "LinAlg/abs.tpp"
 #include "LinAlg/adjoint.tpp"
 #include "LinAlg/conj.tpp"
-#include "LinAlg/det.hpp"
-#include "LinAlg/transpose.hpp"
-#include "LinAlg/exp.hpp"
-#include "LinAlg/mat_exp.hpp"
-#include "LinAlg/mat_mul.hpp"
-#include "LinAlg/mat_vec.hpp"
+#include "LinAlg/det.tpp"
+#include "LinAlg/transpose.tpp"
+#include "LinAlg/exp.tpp"
+#include "LinAlg/mat_exp.tpp"
+#include "LinAlg/mat_mul.tpp"
+#include "LinAlg/mat_vec.tpp"
 
 #endif
