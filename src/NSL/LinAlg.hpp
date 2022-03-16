eb30f02de8746148381c5a169e453efd675d2b61
Marcel Rodekamp <marcel.rodekamp@gmail.com>
2022-03-16 16:02:43 +0100

    Moved adjoint.hpp -> adjoint.tpp and worked in the new Tensor

--- src/NSL/LinAlg.hpp
+++ src/NSL/LinAlg.hpp
@@ -2,10 +2,10 @@
 #define NSL_LINALG_INCLUDE_HPP
 
 #include "LinAlg/abs.tpp"
+#include "LinAlg/adjoint.tpp"
 #include "LinAlg/conj.hpp"
 #include "LinAlg/det.hpp"
 #include "LinAlg/transpose.hpp"
-#include "LinAlg/adjoint.hpp"
 #include "LinAlg/exp.hpp"
 #include "LinAlg/mat_exp.hpp"
 #include "LinAlg/mat_mul.hpp"
