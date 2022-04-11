f0c97e0f8493437cef34a48310702a228a76424d
Evan Berkowitz <git@evanberkowitz.com>
2022-04-11 15:28:43 +0200

    Generic M_dense

--- src/NSL/FermionMatrix/fermionMatrix.hpp
+++ src/NSL/FermionMatrix/fermionMatrix.hpp
@@ -69,6 +69,37 @@ class FermionMatrix {
         Lat(lat)
     {}
 
+    // Construction
+    // Returns a tensor with dimensions (nt, nx, nt, nx) where
+    //  - the first  two are the row,
+    //  - the latter two are the column.
+    // Of course, you should avoid constructing the dense matrix while in production unless absolutely necessary!
+    // TODO: I'm not sure this should actually be a method of the root-level fermionMatrix class, since it assumes that a configuration is of shape (nt, nt) and no other internal indices.
+    NSL::Tensor<Type> M_dense(NSL::size_t nt){
+        // We construct a dense operator generically by applying the mandatory M routine nt * nx times.
+        // We leverage the fact that
+        //      M_{tx,iy} = M_{tx,jz} * δ_{ji} δ_{zy}
+        // On the left-hand side we have a dense matrix;
+        // we get the right-hand side by mat-vec on nt * nx vectors labeled by iy.
+
+        // First we construct the identity matrix.
+        auto nx = this->Lat.sites();
+        NSL::Tensor<Type> identity(nt, nx, nt, nx);
+        for(int t = 0; t < nt; t++){
+            identity(t,NSL::Slice(), t, NSL::Slice()) = NSL::Matrix::Identity<Type>(nx);
+        }
+
+        // Then we apply M to each column.
+        NSL::Tensor<Type> M(nt, nx, nt, nx);
+        for(int i = 0; i < nt; i++){
+            for(int y = 0; y < nx; y++){
+                M(NSL::Slice(), NSL::Slice(), i, y) = this->M(identity(NSL::Slice(), NSL::Slice(), i, y));
+            }
+        }
+
+        return M;
+    }
+
     protected:
     //! An object of Lattice type (Ring, square, etc.).
     LatticeType & Lat;
