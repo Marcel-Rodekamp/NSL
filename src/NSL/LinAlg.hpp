4c349d1dbafc43f3c1a6b63dbbfb1ec9043b20fb
Marcel Rodekamp <marcel.rodekamp@gmail.com>
2022-06-14 09:36:38 +0200

    Added an inner product function to NSL::LinAlg

--- src/NSL/LinAlg.hpp
+++ src/NSL/LinAlg.hpp
@@ -8,6 +8,7 @@
 #include "LinAlg/det.tpp"
 #include "LinAlg/transpose.tpp"
 #include "LinAlg/exp.tpp"
+#include "LinAlg/inner_product.tpp"
 #include "LinAlg/mat_exp.tpp"
 #include "LinAlg/mat_mul.tpp"
 #include "LinAlg/mat_vec.tpp"
