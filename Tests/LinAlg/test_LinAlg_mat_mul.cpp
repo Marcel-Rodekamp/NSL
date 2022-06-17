db1b1faa83fad46ef272a077f75e5b199654dd05
Evan Berkowitz <git@evanberkowitz.com>
2022-06-17 11:25:43 +0200

    Harmonize test cases
     - use NSL::Matrix::Identity when possible.
     - do forward declarations and then implementations at the end.
     - add the LinAlg specifier to the name.

--- Tests/LinAlg/test_LinAlg_mat_mul.cpp
+++ Tests/LinAlg/test_LinAlg_mat_mul.cpp
@@ -4,6 +4,66 @@
 using size_type = long int;
 
 template<typename T>
+void test_mat_mul_zero(const size_type & size);
+
+template<typename T>
+void test_mat_mul_one(const size_type & size);
+
+template<typename T>
+void test_mat_mul_inverse(const size_type & size);
+
+template<typename T>
+void test_stacked_mat_mul_zero(const size_type & size);
+
+template<typename T>
+void test_stacked_mat_mul_one(const size_type & size);
+
+template<typename T>
+void test_stacked_mat_mul_inverse(const size_type & size);
+
+
+// =============================================================================
+// Test Cases
+// =============================================================================
+
+FLOAT_NSL_TEST_CASE( "LinAlg: Matrix Multiplication -- zero", "[LinAlg,mat_mul,zero]" ) {
+    const NSL::size_t size = GENERATE(1, 100, 200, 500, 1000);
+    test_mat_mul_zero<TestType>(size);
+}
+
+
+FLOAT_NSL_TEST_CASE( "LinAlg: Matrix Multiplication -- one", "[LinAlg,mat_mul,one]" ) {
+    const NSL::size_t size = GENERATE(1, 100, 200, 500, 1000);
+    test_mat_mul_one<TestType>(size);
+}
+
+FLOAT_NSL_TEST_CASE( "LinAlg: Matrix Multiplication -- inverses ", "[LinAlg,mat_mul,inverses]" ) {
+    const NSL::size_t size = GENERATE(1, 100, 200, 500, 1000);
+    test_mat_mul_inverse<TestType>(size);
+}
+
+FLOAT_NSL_TEST_CASE( "LinAlg: Matrix Multiplication -- zero(stacked)", "[LinAlg,mat_mul,zero,stacked]" ) {
+    const NSL::size_t size = GENERATE(1, 100, 200, 500, 1000);
+    test_stacked_mat_mul_zero<TestType>(size);
+}
+
+
+FLOAT_NSL_TEST_CASE( "LinAlg: Matrix Multiplication -- one(stacked)", "[LinAlg,mat_mul,one,stacked]" ) {
+    const NSL::size_t size = GENERATE(1, 100, 200, 500, 1000);
+    test_stacked_mat_mul_one<TestType>(size);
+}
+
+FLOAT_NSL_TEST_CASE( "LinAlg: Matrix Multiplication -- inverse(stacked)", "[LinAlg,mat_mul,inverses,stacked]" ) {
+    const NSL::size_t size = GENERATE(1, 100, 200, 500, 1000);
+    test_stacked_mat_mul_inverse<TestType>(size);
+}
+
+// =============================================================================
+// Implementations
+// =============================================================================
+
+
+template<typename T>
 void test_mat_mul_zero(const size_type & size){
     NSL::Tensor<T> t(size, size);
     t.rand();
@@ -23,10 +83,7 @@ void test_mat_mul_one(const size_type & size){
     NSL::Tensor<T> t(size, size);
     t.rand();
 
-    NSL::Tensor<T> one_t(size, size);
-    for(int i = 0; i < size; ++i) {
-        one_t(i,i) = 1.;
-    }
+    NSL::Tensor<T> one_t = NSL::Matrix::Identity<T>(size);
 
     NSL::Tensor<T> one_mul_left = NSL::LinAlg::mat_mul(one_t, t);
     NSL::Tensor<T> one_mul_right = NSL::LinAlg::mat_mul(t, one_t);
@@ -80,9 +137,7 @@ void test_stacked_mat_mul_one(const size_type & size){
 
     NSL::Tensor<T> one_t(10, size, size);
     for(int k = 0; k < 10; ++k) {
-        for(int i = 0; i < size; ++i) {
-            one_t(k,i,i) = 1.;
-        }
+        one_t(k,NSL::Slice(), NSL::Slice()) = NSL::Matrix::Identity<T>(size);
     }
 
     NSL::Tensor<T> one_mul_left = NSL::LinAlg::mat_mul(one_t, t);
@@ -98,14 +153,14 @@ void test_stacked_mat_mul_inverse(const size_type & size){
     NSL::Tensor<T> t(10, size, size);
     for(int k = 0; k < 10; ++k) {
         for(int i = 0; i < size; ++i) {
-            t(k,i,i) = i+1;
+            t(k,i,i) = (i+1)*(k+1);
         }
     }
 
     NSL::Tensor<T> t_inv(10, size, size);
     for(int k = 0; k < 10; ++k) {
         for(int i = 0; i < size; ++i) {
-            t_inv(k,i,i) = 1./(i+1);
+            t_inv(k,i,i) = 1./(i+1)/(k+1);
         }
     }
 
@@ -123,38 +178,3 @@ void test_stacked_mat_mul_inverse(const size_type & size){
 }
 
 
-// =============================================================================
-// Test Cases
-// =============================================================================
-
-FLOAT_NSL_TEST_CASE( "LinAlg: Matrix Multiplication -- zero", "[LinAlg,mat_mul,zero]" ) {
-    const NSL::size_t size = GENERATE(1, 100, 200, 500, 1000);
-    test_mat_mul_zero<TestType>(size);
-}
-
-
-FLOAT_NSL_TEST_CASE( "LinAlg: Matrix Multiplication -- one", "[LinAlg,mat_mul,one]" ) {
-    const NSL::size_t size = GENERATE(1, 100, 200, 500, 1000);
-    test_mat_mul_one<TestType>(size);
-}
-
-FLOAT_NSL_TEST_CASE( "LinAlg: Matrix Multiplication -- inverses ", "[LinAlg,mat_mul,inverses]" ) {
-    const NSL::size_t size = GENERATE(1, 100, 200, 500, 1000);
-    test_mat_mul_inverse<TestType>(size);
-}
-
-FLOAT_NSL_TEST_CASE( "LinAlg: Matrix Multiplication -- zero(stacked)", "[LinAlg,mat_mul,zero,stacked]" ) {
-    const NSL::size_t size = GENERATE(1, 100, 200, 500, 1000);
-    test_stacked_mat_mul_zero<TestType>(size);
-}
-
-
-FLOAT_NSL_TEST_CASE( "LinAlg: Matrix Multiplication -- one(stacked)", "[LinAlg,mat_mul,one,stacked]" ) {
-    const NSL::size_t size = GENERATE(1, 100, 200, 500, 1000);
-    test_stacked_mat_mul_one<TestType>(size);
-}
-
-FLOAT_NSL_TEST_CASE( "LinAlg: Matrix Multiplication -- inverse(stacked)", "[LinAlg,mat_mul,inverses,stacked]" ) {
-    const NSL::size_t size = GENERATE(1, 100, 200, 500, 1000);
-    test_stacked_mat_mul_inverse<TestType>(size);
-}
