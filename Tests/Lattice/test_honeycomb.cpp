15e9d0cbcbd29e09c87864a052dd1a01bbbdff2e
Evan Berkowitz <git@evanberkowitz.com>
2023-05-02 16:14:31 +0200

    Spectral tests for the honeycomb lattice!
     - The energy eigenvalues are always between ±3.
     - The 2 Dirac points give 4 zero energies when L1 and L2 == 0 (mod 3).
    
    Still outstanding: we should have tests of all comparisons; see #133,
    which arose from two logically equivalent formulations of not_too_small
    giving different results.

--- Tests/Lattice/test_honeycomb.cpp
+++ Tests/Lattice/test_honeycomb.cpp
@@ -1,7 +1,7 @@
 #include "../test.hpp"
 
-/*! \file test_square.cpp
- *  Test the `Lattice::Square` implementation.
+/*! \file test_honeycomb.cpp
+ *  Test the `Lattice::Honeycomb` implementation.
  */
 
 //! Torch requirement
@@ -33,14 +33,60 @@ void test_honeycomb_hermiticity(NSL::Lattice::Honeycomb<T> & lattice){
     REQUIRE( ((K - K.conj().transpose(0,1)) == 0).all() );
 }
 
+template<typename T>
+void test_honeycomb_eigenvector_orthogonality(const NSL::Tensor<T> & eigenvectors){
+    
+    NSL::size_t n = eigenvectors.shape(0);
+    NSL::Tensor<T> one = NSL::Matrix::Identity<T>(n);
+
+    NSL::Tensor<T> dot_products = NSL::LinAlg::mat_mul(NSL::LinAlg::transpose(eigenvectors).conj(), eigenvectors);
+    REQUIRE( almost_equal(one, dot_products).all() );
+}
+
+template<typename T>
+void test_honeycomb_spectrum_limits(const NSL::Tensor<T> & eigenvalues, const double epsilon=1.e-6){
+
+    double limit = 3 + epsilon;
+
+    INFO("Limit = " << limit);
+    
+    auto not_too_big   = (eigenvalues <= +limit);
+    //auto not_too_small = (-limit <= eigenvalues); // See https://github.com/Marcel-Rodekamp/NSL/issues/133
+    auto not_too_small = (eigenvalues >= -limit);
+
+    INFO("Eigenvalues <= +3" << not_too_big);
+    REQUIRE( not_too_big.all() );
+
+    INFO("-3 <= Eigenvalues" << not_too_small);
+    REQUIRE( not_too_small.all() );
+
+}
+
+template<typename T>
+void test_honeycomb_dirac_points(const size_type & L1, const size_type & L2, const NSL::Tensor<T> & eigenvalues){
+
+    // We have to cast to into to get a correct sum.
+    int zero_energy_states = NSL::Tensor<int>(almost_equal(eigenvalues, T(0))).sum();
+
+    // If (L1, L2) == (0,0) mod 3 we get 2 Dirac points.
+    // Each Dirac point has two energy eigenvectors; one of each band.
+    // So if there are Dirac points we expect to see 4 zero-energy states; and 0 otherwise.
+    if( (L1 % 3 == 0) && (L2 % 3 == 0) ){
+        REQUIRE( zero_energy_states == 4 );
+    } else {
+        REQUIRE( zero_energy_states == 0 );
+    }
+
+}
+
 // =============================================================================
 // Test Cases
 // =============================================================================
 
 REAL_NSL_TEST_CASE( "Lattice: Honeycomb", "[Lattice, Honeycomb]" ) {
-    const size_type L1 = GENERATE(2, 3, 4, 6, 12, 15);
-    //const size_type L2 = GENERATE(2, 3, 4, 6, 12, 15);
-    const size_type L2 = L1;
+    const size_type L1 = GENERATE(2, 3, 4, 5, 6, 9, 12);
+    const size_type L2 = GENERATE(2, 3, 4, 5, 6, 9, 12);
+    //const size_type L2 = L1;
 
     std::vector<size_type> L(2);
     L[0] = L1;
@@ -50,15 +96,28 @@ REAL_NSL_TEST_CASE( "Lattice: Honeycomb", "[Lattice, Honeycomb]" ) {
     INFO("L    = " << L);
 
     NSL::Lattice::Honeycomb<TestType> lattice(L);
-
+    auto [energies, vectors]  = NSL::LinAlg::eigh(lattice.hopping_matrix(1.0));
+    
     INFO(lattice.name());
-    INFO(lattice.sites());
-    INFO(lattice.coordinates());
-    INFO(lattice.adjacency_matrix());
+    INFO("Sites = " << lattice.sites());
+
+    INFO("Coordinates")
+	INFO("-----------");
+	INFO(lattice.coordinates());
+	INFO("-----------");
+	
+    // INFO("Adjacency matrix:\n" << lattice.adjacency_matrix());
+	
+    INFO("Energy eigenvalues");
+    INFO("------------------");
+    INFO(energies);
+    INFO("------------------");
     
     test_honeycomb_volume<TestType>(lattice);
     test_honeycomb_valence<TestType>(lattice);
     test_honeycomb_hermiticity<TestType>(lattice);
-    //! \todo: Add a test of the spectrum; Dirac points should only show up when L%3 = 0.
+    test_honeycomb_eigenvector_orthogonality(vectors);
+    test_honeycomb_spectrum_limits(energies);
+    test_honeycomb_dirac_points(L1, L2, energies);
 }
 
