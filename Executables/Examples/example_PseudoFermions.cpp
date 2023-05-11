b77ceccd7f0ec2b0dd2df1ed8de22450bde036d6
Chrisgant <cgantgen@gmail.com>
2023-05-11 09:34:53 +0200

    Example to test Pseudo-Leapfrog

---
+++ Executables/Examples/example_PseudoFermions.cpp
@@ -0,0 +1,80 @@
+#include "Action/Implementations/hubbardGaugeAction.tpp"
+#include "Action/Implementations/hubbardFermiAction.tpp"
+#include "Integrator/Impl/leapfrog.tpp"
+#include "NSL.hpp"
+
+int main(){
+    
+    typedef NSL::complex<double> cd;
+	
+    NSL::size_t Nx =  8;
+    NSL::size_t Nt =  32;
+    NSL::Tensor<cd> phi(Nt,Nx); phi.randn(); 
+    NSL::Tensor<cd> pi(Nt,Nx); pi.randn();
+ 
+    NSL::Lattice::Ring<cd> lattice(Nx); 
+
+    phi.imag() = 0;
+    pi.imag() = 0;
+	//xi stays complex
+   
+    // define configuration
+    NSL::Configuration<cd> config{
+		{"phi",phi},
+    };
+
+    cd beta = 10.0;
+    cd U = 3.0;
+    NSL::Action::HubbardPseudoFermionAction<cd,decltype(lattice),
+        NSL::FermionMatrix::HubbardExp<cd,decltype(lattice)>>::Parameters paramsHFM(
+        /*beta=*/  beta,
+        /*Nt = */  Nt,    
+        /*lattice=*/lattice
+        );
+    
+    // define momentum
+    NSL::Configuration<cd> momentum{
+		{"phi",pi}, 
+	};
+
+    // define the parameters for the action
+    NSL::Action::HubbardGaugeAction<cd>::Parameters params(
+        /*beta=*/  beta,
+        /*Nt = */  Nt,    
+        /*U =  */  U
+    );
+
+    NSL::Action::HubbardGaugeAction<cd> S_gauge(params);
+    NSL::Action::HubbardPseudoFermionAction<cd,decltype(lattice),NSL::FermionMatrix::HubbardExp<cd,decltype(lattice)>> S_Pfermion(paramsHFM);
+    // define the action
+	// for(unsigned int i=0; i<200;i++){
+    // 	S_Pfermion.update();
+	// }
+	S_Pfermion.update();
+	// std::cout << S_Pfermion.xi.imag() << std::endl;
+	// std::cout << S_Pfermion.xi.imag() << std::endl;
+	NSL::Action::Action S = S_gauge + S_Pfermion;
+
+    cd Hi, Hf;
+
+    Hi = (momentum["phi"] * momentum["phi"]).sum()/2.0 + S(config);
+int Nmd = 5;
+    // for (int Nmd = 10; Nmd < 210; Nmd += 10){
+      // define integrator
+      NSL::Integrator::Leapfrog LF(
+        /*action=*/ S,
+        /*trajectoryLength=*/ 0.1,
+        /*numberSteps=*/ Nmd,
+        /*backward*/ false // optional
+      );
+
+      // integrate eom
+      auto [config_proposal,momentum_proposal] = LF(/*q=*/config,/*p*/ momentum);
+ 
+      Hf = (momentum_proposal["phi"] * momentum_proposal["phi"]).sum()/2.0 + S(config_proposal);
+      std::cout << Nmd << "\t" << NSL::LinAlg::abs((Hf-Hi).real()/Hi.real()) << std::endl;
+      std::cout << Hf << Hi << std::endl;
+    // }
+
+    return EXIT_SUCCESS;
+}
