ca0ac8d157a16c08b40e5ad3fae5857b1faff699
Petar Sinilkov <73816957+petio9671@users.noreply.github.com>
2024-10-22 16:20:12 +0200

    Now the autotuner will continue into production seamlessly

--- autotuner8/autotuner_hasenbusch.py
+++ autotuner8/autotuner_hasenbusch.py
@@ -56,6 +56,7 @@ import argparse
 parser = argparse.ArgumentParser() #help='Process data down to a manageable size by cutting, binning, and bootstrapping.'
 parser.add_argument("yaml", type=str, help="Initialization YAML file")
 parser.add_argument("executable", type=str, help="NSL Executable")
+parser.add_argument("--GPU", default=False, action='store_true', help="Using GPU")
 args = parser.parse_args()
 
 with open(args.yaml) as stream:
@@ -81,11 +82,13 @@ all_steps = []
 last_counter = 0
 all_accept_rates = dict()
 
+tuneFlag = 0
 os.system(f"mkdir -p {graph_dir}")
 
 if os.path.exists(ymlFile["fileIO"]["h5file"]):
     with h5.File(ymlFile["fileIO"]["h5file"], 'r') as f:
         try:
+            tuneFlag = f[f"{base_name}/Meta/params/tuneFlag"][()]
             steps = f[f"{base_name}/Meta/params/nMD"][()]
             stage_counter = f[f"{base_name}/autotune/stage-counter"][()]
             last_guess = f[f"{base_name}/autotune/last-guess"][()]
@@ -104,158 +107,164 @@ if os.path.exists(ymlFile["fileIO"]["h5file"]):
 
         graph_counter = len(all_steps)
 
-for counter in range(max_counter):
+if tuneFlag == 0:
 
-    if counter < int(last_counter):
-        # Skip until the last saved counter when resuming autotuning
-        continue
+    for counter in range(max_counter):
 
-    # Run
-    # p.cpu_affinity(cpuList)
-    result = subprocess.run([f"{binary}", "--file", f"{args.yaml}"], check=True)
-    print(result)
+        if counter < int(last_counter):
+            # Skip until the last saved counter when resuming autotuning
+            continue
 
-    # os.system(f"./{binary} --file {args.yaml}")
-    # p.cpu_affinity([cpuList[0]])
-    trajectories_count += 1
-    counter += 1
+        # Run
+        # p.cpu_affinity(cpuList)
+        result = subprocess.run([f"{binary}", "--file", f"{args.yaml}"], check=True)
+        # result = subprocess.run([f"{binary}", "--file", f"{args.yaml}", *(["--GPU"] if args.GPU else []) ], check=True)
+        print(result)
 
-    # Count accepted traj.
-    probabilities = []
-    accepted_count = 0
-    accepted_count_total = 0
-    accepted_prob_total = 0
+        # os.system(f"./{binary} --file {args.yaml}")
+        # p.cpu_affinity([cpuList[0]])
+        trajectories_count += 1
+        counter += 1
+
+        # Count accepted traj.
+        probabilities = []
+        accepted_count = 0
+        accepted_count_total = 0
+        accepted_prob_total = 0
+
+        with h5.File(ymlFile["fileIO"]["h5file"], 'a') as f:
+            if f[f"{base_name}/Meta/params/thermalFlag"][()] == 0:
+                f[f"{base_name}/Meta/params/thermalFlag"][...] = 1
+
+            for traj in range(counter):
+                acceptance_probability = min(1, f[f"{base_name}/{conf_dir}/{traj}/acceptanceProbability"][()])
+                acceptance_rate = f[f"{base_name}/{conf_dir}/{traj}/acceptanceRate"][()]
+
+                if traj >= counter - trajectories_count:
+                    probabilities.append(acceptance_probability)
+                    accepted_count += acceptance_rate
+
+                accepted_prob_total += acceptance_probability
+                accepted_count_total += acceptance_rate
+
+        accept_rate = accepted_count/trajectories_count
+        accept_rate_total = accepted_count_total/counter
+        accept_rate_prob_total = accepted_prob_total / counter
+        conf_int = wald_interval(accepted_count, trajectories_count, 0.95)
+        
+        probabilities = np.array(probabilities)
+        accept_rate_prob = probabilities.mean()
+        conf_int_prob = conf_int_N_draws(
+            accept_rate_prob, probabilities.std(), len(probabilities), 0.95)
+
+        info_string = "Traj: %d  acc.r.: (bin)%f (prob)%f | Current: steps: %d  traj: %d  acc.r/conf.int: (bin)%f/%f(%f,%f) (prob)%f/%f(%f,%f)"
+        values_to_print = (counter, accept_rate_total, accept_rate_prob_total, steps, trajectories_count,
+                        accept_rate, conf_int[0], conf_int[1], conf_int[2], accept_rate_prob, conf_int_prob[0], conf_int_prob[1], conf_int_prob[2])
+        print(info_string % values_to_print)
+        
+        # Try to change the number of steps
+        if trajectories_count >= min_trajectories and ((conf_int_prob[0] < significance_interval) or (conf_int[0] < significance_interval*0.2) or (trajectories_count >= max_trajectories)):
+            if conf_int_prob[0] < significance_interval and optimal_acceptance >= conf_int_prob[1] and optimal_acceptance <= conf_int_prob[2]:
+                counter_stopping_condition += 1
+            trajectories_count = 0
+
+            all_steps.append(steps)
+            all_accept_rates[steps] = accept_rate_prob
+
+            # Insert two marginal artifitial points to the training data
+            independent_var = np.array(
+                list(all_accept_rates.keys())+[1.0, 2000], dtype=float)  # Number of md steps
+            dependent_var = np.array(
+                list(all_accept_rates.values())+[0.0, 1.0], dtype=float)  # Accept rates
+
+            # Initial guesses for fitting, e.g based on a result of a small lattice
+            guesses = []
+            guesses.append(
+                np.array([-9.27128658e+01,   2.16389117e-02,   7.24033137e-01]))
+            guesses.append(np.array([2e-1 for i in range(3)]))
+            try:
+                if last_guess != None:
+                    guesses.append(last_guess)
+            except ValueError:
+                guesses.append(last_guess)
+
+            # Utilize the Levenberg-Marquardt algorithm by using the scipy library
+            best_res = least_squares(function, guesses[0], ftol=1e-10, gtol=1e-10, xtol=1e-10, args=(
+                independent_var, dependent_var), verbose=fitting_verbose_level, loss='soft_l1', max_nfev=1000)
+            for x0 in guesses[1:]:
+                res = least_squares(function, x0, ftol=1e-10, gtol=1e-10, xtol=1e-10, args=(
+                    independent_var, dependent_var), verbose=fitting_verbose_level, loss='soft_l1', max_nfev=1000)
+                if res.cost < best_res.cost:
+                    best_res = res
+            last_guess = res.x
+
+            # Get a set of predictions from the model
+            u_test = np.linspace(hasenbusch_timescale, 2000, 20000)
+            y_test = model_to_fit(best_res.x, u_test)
+
+            # Plot fitting graphs
+            if enable_graphs is True:
+                pl.plot(independent_var[:-2], dependent_var[:-2],
+                        'o', markersize=4, label='data')
+                #pl.plot(independent_var[-2:], dependent_var[-2:], 'o', markersize=4, label='art. data')
+                pl.plot(u_test, y_test, label='fitted model')
+                pl.xlabel('Number of steps')
+                pl.ylabel('Acceptance rate')
+                pl.ylim(-0.2, 1.2)
+                pl.xlim(-2,202)
+                pl.legend(loc='lower right')
+                pl.grid(color='#dddddd', linestyle='--', linewidth=0.5)
+
+                pl.gcf().set_size_inches(12.14, 7.5)
+                pl.savefig(f"{graph_dir}/{ymlFile['fileIO']['h5file'][:-3]}-{str(graph_counter).zfill(2)}.png", dpi=None, facecolor='w', edgecolor='w',
+                        format='png', transparent=False, bbox_inches=None, pad_inches=0.1)#, rameon=None)
+                pl.clf()
+                graph_counter += 1
+
+            # Nmd selection based on the model
+            next_steps = steps
+            diff = abs(accept_rate_prob-optimal_acceptance)
+            for step in range(len(u_test)):
+                if abs(y_test[step]-optimal_acceptance) < diff and (u_test[step] > steps or optimal_acceptance < conf_int_prob[2]):
+                    diff = abs(y_test[step]-optimal_acceptance)
+                    next_steps = int(
+                        u_test[step]/hasenbusch_timescale+0.5)*hasenbusch_timescale
+            steps = next_steps
+            print("New Nmd selected from model: ".upper() + str(steps))
+            with h5.File(ymlFile["fileIO"]["h5file"], 'a') as f:
+                f[f"{base_name}/Meta/params/nMD"][...] = steps
+            if counter_stopping_condition >= 3 or (all_steps.count(steps) > 3 and optimal_acceptance < conf_int_prob[2]):
+                if stage_counter != 0:
+                    print("Stopping condition already met.")
+                    break
+                stage_counter += 1
+
+                with h5.File(ymlFile["fileIO"]["h5file"], 'a') as f:
+                    f[f"{base_name}/Meta/params/thermalFlag"][...] = 0
+                    f.create_group(f"{base_name}/autotune/all-accept-rates")
+                    f[f"{base_name}/autotune/stage-counter"] = stage_counter
+                    f[f"{base_name}/autotune/last-guess"] = np.asarray(last_guess)
+                    for k,v in all_accept_rates.items():
+                        f[f"{base_name}/autotune/all-accept-rates"].create_dataset(str(k), data=v)
+
+                result = subprocess.run([f"{binary}", "--file", f"{args.yaml}"], check=True)
+                print(result)
+
+                with h5.File(ymlFile["fileIO"]["h5file"], 'a') as f:
+                    f[f"{base_name}/Meta/params/thermalFlag"][...] = 1
+                    print(f[f"{base_name}/Meta/params/thermalFlag"][...])
+
+                counter_stopping_condition = 0
+                all_steps = []
+                min_trajectories = 7
+                max_trajectories = 50
+                significance_interval = 0.20
+
 
     with h5.File(ymlFile["fileIO"]["h5file"], 'a') as f:
-        if f[f"{base_name}/Meta/params/thermalFlag"][()] == 0:
-            f[f"{base_name}/Meta/params/thermalFlag"][...] = 1
+        f[f"{base_name}/Meta/params/tuneFlag"][...] = 1
 
-        for traj in range(counter):
-            acceptance_probability = min(1, f[f"{base_name}/{conf_dir}/{traj}/acceptanceProbability"][()])
-            acceptance_rate = f[f"{base_name}/{conf_dir}/{traj}/acceptanceRate"][()]
+    print("Final Nmd: " + str(steps))
 
-            if traj >= counter - trajectories_count:
-                probabilities.append(acceptance_probability)
-                accepted_count += acceptance_rate
-
-            accepted_prob_total += acceptance_probability
-            accepted_count_total += acceptance_rate
-
-    accept_rate = accepted_count/trajectories_count
-    accept_rate_total = accepted_count_total/counter
-    accept_rate_prob_total = accepted_prob_total / counter
-    conf_int = wald_interval(accepted_count, trajectories_count, 0.95)
-    
-    probabilities = np.array(probabilities)
-    accept_rate_prob = probabilities.mean()
-    conf_int_prob = conf_int_N_draws(
-        accept_rate_prob, probabilities.std(), len(probabilities), 0.95)
-
-    info_string = "Traj: %d  acc.r.: (bin)%f (prob)%f | Current: steps: %d  traj: %d  acc.r/conf.int: (bin)%f/%f(%f,%f) (prob)%f/%f(%f,%f)"
-    values_to_print = (counter, accept_rate_total, accept_rate_prob_total, steps, trajectories_count,
-                       accept_rate, conf_int[0], conf_int[1], conf_int[2], accept_rate_prob, conf_int_prob[0], conf_int_prob[1], conf_int_prob[2])
-    print(info_string % values_to_print)
-    
-    # Try to change the number of steps
-    if trajectories_count >= min_trajectories and ((conf_int_prob[0] < significance_interval) or (conf_int[0] < significance_interval*0.2) or (trajectories_count >= max_trajectories)):
-        if conf_int_prob[0] < significance_interval and optimal_acceptance >= conf_int_prob[1] and optimal_acceptance <= conf_int_prob[2]:
-            counter_stopping_condition += 1
-        trajectories_count = 0
-
-        all_steps.append(steps)
-        all_accept_rates[steps] = accept_rate_prob
-
-        # Insert two marginal artifitial points to the training data
-        independent_var = np.array(
-            list(all_accept_rates.keys())+[1.0, 2000], dtype=float)  # Number of md steps
-        dependent_var = np.array(
-            list(all_accept_rates.values())+[0.0, 1.0], dtype=float)  # Accept rates
-
-        # Initial guesses for fitting, e.g based on a result of a small lattice
-        guesses = []
-        guesses.append(
-            np.array([-9.27128658e+01,   2.16389117e-02,   7.24033137e-01]))
-        guesses.append(np.array([2e-1 for i in range(3)]))
-        try:
-            if last_guess != None:
-                guesses.append(last_guess)
-        except ValueError:
-            guesses.append(last_guess)
-
-        # Utilize the Levenberg-Marquardt algorithm by using the scipy library
-        best_res = least_squares(function, guesses[0], ftol=1e-10, gtol=1e-10, xtol=1e-10, args=(
-            independent_var, dependent_var), verbose=fitting_verbose_level, loss='soft_l1', max_nfev=1000)
-        for x0 in guesses[1:]:
-            res = least_squares(function, x0, ftol=1e-10, gtol=1e-10, xtol=1e-10, args=(
-                independent_var, dependent_var), verbose=fitting_verbose_level, loss='soft_l1', max_nfev=1000)
-            if res.cost < best_res.cost:
-                best_res = res
-        last_guess = res.x
-
-        # Get a set of predictions from the model
-        u_test = np.linspace(hasenbusch_timescale, 2000, 20000)
-        y_test = model_to_fit(best_res.x, u_test)
-
-        # Plot fitting graphs
-        if enable_graphs is True:
-            pl.plot(independent_var[:-2], dependent_var[:-2],
-                    'o', markersize=4, label='data')
-            #pl.plot(independent_var[-2:], dependent_var[-2:], 'o', markersize=4, label='art. data')
-            pl.plot(u_test, y_test, label='fitted model')
-            pl.xlabel('Number of steps')
-            pl.ylabel('Acceptance rate')
-            pl.ylim(-0.2, 1.2)
-            pl.xlim(-2,202)
-            pl.legend(loc='lower right')
-            pl.grid(color='#dddddd', linestyle='--', linewidth=0.5)
-
-            pl.gcf().set_size_inches(12.14, 7.5)
-            pl.savefig(f"{graph_dir}/{ymlFile['fileIO']['h5file'][:-3]}-{str(graph_counter).zfill(2)}.png", dpi=None, facecolor='w', edgecolor='w',
-                       format='png', transparent=False, bbox_inches=None, pad_inches=0.1)#, rameon=None)
-            pl.clf()
-            graph_counter += 1
-
-        # Nmd selection based on the model
-        next_steps = steps
-        diff = abs(accept_rate_prob-optimal_acceptance)
-        for step in range(len(u_test)):
-            if abs(y_test[step]-optimal_acceptance) < diff and (u_test[step] > steps or optimal_acceptance < conf_int_prob[2]):
-                diff = abs(y_test[step]-optimal_acceptance)
-                next_steps = int(
-                    u_test[step]/hasenbusch_timescale+0.5)*hasenbusch_timescale
-        steps = next_steps
-        print("New Nmd selected from model: ".upper() + str(steps))
-        with h5.File(ymlFile["fileIO"]["h5file"], 'a') as f:
-            f[f"{base_name}/Meta/params/nMD"][...] = steps
-        if counter_stopping_condition >= 3 or (all_steps.count(steps) > 3 and optimal_acceptance < conf_int_prob[2]):
-            if stage_counter != 0:
-                print("Stopping condition already met.")
-                break
-            stage_counter += 1
-
-            with h5.File(ymlFile["fileIO"]["h5file"], 'a') as f:
-                f[f"{base_name}/Meta/params/thermalFlag"][...] = 0
-                f.create_group(f"{base_name}/autotune/all-accept-rates")
-                f[f"{base_name}/autotune/stage-counter"] = stage_counter
-                f[f"{base_name}/autotune/last-guess"] = np.asarray(last_guess)
-                for k,v in all_accept_rates.items():
-                    f[f"{base_name}/autotune/all-accept-rates"].create_dataset(str(k), data=v)
-
-            result = subprocess.run([f"{binary}", "--file", f"{args.yaml}"], check=True)
-            print(result)
-
-            with h5.File(ymlFile["fileIO"]["h5file"], 'a') as f:
-                f[f"{base_name}/Meta/params/thermalFlag"][...] = 1
-                print(f[f"{base_name}/Meta/params/thermalFlag"][...])
-
-            counter_stopping_condition = 0
-            all_steps = []
-            min_trajectories = 7
-            max_trajectories = 50
-            significance_interval = 0.20
-
-
-with h5.File(ymlFile["fileIO"]["h5file"], 'a') as f:
-    f[f"{base_name}/Meta/params/tuneFlag"][...] = 1
-
-print("Final Nmd: " + str(steps))
+result = subprocess.run([f"{binary}", "--file", f"{args.yaml}", *(["--GPU"] if args.GPU else []) ], check=True)
+print(result)
