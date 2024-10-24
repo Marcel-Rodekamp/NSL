a56c33364ac48e1392341cd66ed1aaa6043bb7c3
Christoph Gäntgen <cgantgen@gmail.com>
2024-10-24 11:52:03 +0200

    include missing measurement

--- src/NSL/Measurements.hpp
+++ src/NSL/Measurements.hpp
@@ -3,5 +3,6 @@
 
 #include "Measurements/Hubbard/twoPointCorrelationFunction.tpp"
 #include "Measurements/Hubbard/twoBodyCorrelationFunction.tpp"
+#include "Measurements/U1/wilsonLoop.tpp"
 
 #endif //NSL_MEASURE_HPP
