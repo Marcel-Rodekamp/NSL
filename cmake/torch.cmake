176ffaac4fd89563b84befc50d5934b36eb5da55
Chrisgant <cgantgen@gmail.com>
2024-10-25 15:11:51 +0200

    added USE_CUDA flag, to compile GPU support on cpu only machine (e.g. login node)

--- cmake/torch.cmake
+++ cmake/torch.cmake
@@ -17,11 +17,15 @@ target_link_libraries(NSL
 )
 
 option(USE_CPU "Use CPU instead of GPU" OFF)
-# Check if Torch is built with CUDA support
+option(USE_CUDA "Use GPU instead of CPU" OFF)
 if (USE_CPU)
     message(STATUS "USE_CPU flag is set, disabling GPU optimizations")
     add_definitions(-DUSE_CPU)
+elseif (USE_CUDA)
+    message(STATUS "USE_CUDA flag is set, enabling GPU optimizations")
+    add_definitions(-DUSE_CUDA)
 else()
+    # Check if Torch is built with CUDA support
     if (Torch_FOUND AND Torch_CUDA_VERSION)
         message(STATUS "Torch with CUDA support found, enabling GPU optimizations")
         add_definitions(-DUSE_CUDA)
