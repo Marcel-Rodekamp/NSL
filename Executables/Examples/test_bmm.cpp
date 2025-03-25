52585cd16a1f75421bd293897af1318b0ce6c679
ftemmen <finn.temmen@web.de>
2025-03-25 11:12:44 +0100

    Added Cuda synchronize

--- Executables/Examples/test_bmm.cpp
+++ Executables/Examples/test_bmm.cpp
@@ -79,7 +79,7 @@ int main(int argc, char* argv[]){
         params["Nradial"] = 0;
     }
 
-    params["test_N"] = yml["systen"]["test_N"].as<NSL::size_t>();
+    params["test_N"] = yml["system"]["test_N"].as<NSL::size_t>();
     // std::cout << params["Nx"] << std::endl;
 
     // Now we want to log the found parameters
@@ -156,6 +156,9 @@ int main(int argc, char* argv[]){
 
     // Gradient test
     std::cout << "Forward bmm evaluation" << std::endl;
+    CUDAStream stream = getCurrentCUDAStream();
+    AT_CUDA_CHECK(cudaStreamSynchronize(stream));
+
     auto t3 = std::chrono::high_resolution_clock::now();
     for (int i=0; i < N; i++){
         NSL::Configuration<Type> grad_val = S_fermion.bmmgrad(config["phi"]);
@@ -184,6 +187,8 @@ int main(int argc, char* argv[]){
     }
     auto t2 = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> t = t2 - t1;
+    CUDAStream stream = getCurrentCUDAStream();
+    AT_CUDA_CHECK(cudaStreamSynchronize(stream));
     double t_val = t.count();
     std::cout << "Time: " << t.count() << std::endl;
     double speed_up = t_val/t_bmm_val;
@@ -196,6 +201,8 @@ int main(int argc, char* argv[]){
     // config["phi"].randn();
 
     std::cout << "Forward bmm evaluation" << std::endl;
+    CUDAStream stream = getCurrentCUDAStream();
+    AT_CUDA_CHECK(cudaStreamSynchronize(stream));
     auto t5 = std::chrono::high_resolution_clock::now();
     for (int i=0; i < N; i++){
         Type act_val = S_fermion.bmmeval(config["phi"]);
@@ -224,6 +231,8 @@ int main(int argc, char* argv[]){
     }
     auto t8 = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> tdur2 = t8 - t7;
+    CUDAStream stream = getCurrentCUDAStream();
+    AT_CUDA_CHECK(cudaStreamSynchronize(stream));
     double t_val2 = tdur2.count();
     std::cout << "Time: " << t_val2 << std::endl;
     double speed_up2 = t_val2/t_bmm_val2;
