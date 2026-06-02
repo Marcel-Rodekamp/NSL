cc67d8bfa12b34d6cf3c035ede034704141c5e25
luu1 <luu1@jrlogin04.jureca>
2026-06-02 16:37:14 +0200

    added spatial propagator measurements for spin and charge basis, and converted propgator to momentum basis

--- Executables/Hubbard/propagatorChargeBasis.cpp
+++ Executables/Hubbard/propagatorChargeBasis.cpp
@@ -108,41 +108,25 @@ int main(int argc, char* argv[]){
     // Put the lattice on the device. (copy to GPU)
     lattice.to(params["device"]);
     
-    std::vector<std::vector<std::vector<std::vector<double>>>> kblocks2d;
-    std::vector<std::vector<double>> momenta;
-   
+    // define a hubbard gauge action
+    NSL::Action::HubbardGaugeAction<Type> Sg(params);
 
-    // Load momentum blocks if they exist
-    if (yml["measurements"]["momenta"]){
-      momenta = yml["measurements"]["momenta"].as<std::vector<std::vector<double>>>();
-      NSL::Tensor<double> mblocks(momenta.size(),momenta[0].size());
-      for (int i=0;i<momenta.size(); i++){
-        for (int j=0;j<momenta[0].size();j++){
-          mblocks(i,j) = momenta[i][j];
-        }
-      }
-      params["momenta"]=mblocks;
+    // define a hubbard fermion action, the discretization (HubbardExp) is
+    // hard wired in the meta data if you change this here, also change the
+    // writeMeta()
+    //
+    NSL::Action::HubbardFermionAction<
+        Type, decltype(lattice), NSL::FermionMatrix::HubbardExp<Type,decltype(lattice)>
+      > Sf(lattice,params);
+    
+    // set stability method if defined in yml file, otherwise default is "QR"
+    if (yml["stability"] ) {
+      std::string stabilityMethod = params["stability"];
+      Sf.hfm_.stabilityMethod = stabilityMethod;// "QR", "DIRECTINVERSE", "SVD"
     }
 
-    if (yml["measurements"]["wallSources"]){
-      kblocks2d = yml["measurements"]["wallSources"].as<std::vector<std::vector<std::vector<std::vector<double>>>>>();
-      NSL::Tensor<NSL::complex<double>> kblocks(kblocks2d.size(),kblocks2d[0].size(),kblocks2d[0][0].size());
-      NSL::Logger::info( "Measuring {} momentum block(s), each with {} band(s) of length {}",kblocks2d.size(),kblocks2d[0].size(),kblocks2d[0][0].size());
-
-      // now populate the momentum blocks with proper complex variables
-      for (int i=0; i<kblocks2d.size(); i++){
-        for (int j=0; j<kblocks2d[0].size(); j++) {
-          for (int k=0; k<kblocks2d[0][0].size(); k++) {
-            kblocks(i,j,k) = NSL::complex<double> (kblocks2d[i][j][k][0], kblocks2d[i][j][k][1]);
-          }
-        }
-      }
-      params["wallSources"]=kblocks.to(params["device"]);
-    } else {
-      // DEFAULT: raise an exception
-      // currently don't know how to do this, will do later
-    }
-
+    // Initialize the action being the sum of the gauge action & fermion action
+    NSL::Action::Action S = Sg + Sf;
 
     NSL::size_t Nx =  NSL::size_t(params["Nx"]);
     NSL::size_t Nt =  NSL::size_t(params["Nt"]);
@@ -186,16 +170,9 @@ int main(int argc, char* argv[]){
             Type,decltype(lattice)
         >
     > invM(lattice, params, h5);
-
-    // set stability method if defined in yml file, otherwise default is "QR"
-    if (yml["stability"] ) {
-      std::string stabilityMethod = params["stability"];
-      invM.hfm_.stabilityMethod = stabilityMethod; // "QR", "DIRECTINVERSE", "SVD"
-    }
-
+    
     // Perform calculation of propagator components
-    invM.measureK();
+    invM.measure();
 
     return EXIT_SUCCESS;
 }
-
